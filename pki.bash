53cbc699756be661fb044c86392c7f6766678bd0
Florian Hotze <florianh_dev@icloud.com>
2024-09-08 14:26:38 +0200

    Initial commit

---
+++ pki.bash
@@ -0,0 +1,233 @@
+#!/bin/bash
+
+## Private: Setup the root CA
+#
+setup_root_ca() {
+  echo "Generating root CA CSR..."
+  openssl req -new \
+    -config etc/root-ca.conf \
+    -out ca/root-ca.csr \
+    -keyout ca/root-ca/private/root-ca.key
+  echo "Signing root CA CSR..."
+  openssl ca -selfsign \
+    -config etc/root-ca.conf \
+    -in ca/root-ca.csr \
+    -out ca/root-ca.crt \
+    -extensions root_ca_ext
+}
+
+## Private: Setup an intermediate CS
+#
+# Usage:
+#   setup_intermediate_ca caName
+# Example:
+#   setup_intermediate_ca "signing"
+setup_intermediate_ca() {
+  echo "Generating ${1} intermediate CA CSR..."
+  openssl req -new \
+    -config "etc/${1}-ca.conf" \
+    -out "ca/${1}-ca.csr" \
+    -keyout "ca/${1}-ca/private/${1}-ca.key"
+  echo "Signing ${1} intermediate CA CSR..."
+  openssl ca \
+    -config etc/root-ca.conf \
+    -in "ca/${1}-ca.csr" \
+    -out "ca/${1}-ca.crt" \
+    -extensions "${1}_ca_ext"
+}
+
+## Private: Set the organizationName in the config files
+#
+# Usage:
+#   set_organization_name organizationName
+# Example:
+#   set_organization_name "Florian IT"
+set_organization_name() {
+  find etc -type f -exec sed -i "s/Simple Inc/${1}/g" {} +
+}
+
+## Setup the PKI
+#
+# Usage:
+#   setup organizationName
+# Example
+#   setup "Florian IT"
+setup() {
+  set_organization_name "${1}"
+  setup_root_ca
+  setup_intermediate_ca "signing"
+  setup_intermediate_ca "mtls"
+}
+
+## Private: Sign a TLS server certificate CSR
+#
+# Usage:
+#   sign_server hostname
+# Example:
+#   sign_server "openhab.localnet"
+sign_server() {
+  echo "Signing ${1} server CSR..."
+  openssl ca \
+    -config etc/signing-ca.conf \
+    -in "certs/signing/${FILENAME}.csr" \
+    -out "certs/signing/${FILENAME}.crt" \
+    -extensions server_ext
+  openssl x509 \
+    -in "certs/signing/${FILENAME}.crt" \
+    -out "certs/signing/${FILENAME}.cer" \
+    -outform der
+}
+
+## Create a new TLS server certificate
+#
+# Usage:
+#   create_server hostname ip
+# Example:
+#   create_server "openhab.localnet" "192.168.178.46"
+create_server() {
+  FILENAME="${1/./-}"
+  echo "Generating ${1} server CSR..."
+  DNS="${1}" IP="${2}" \
+  openssl req -new \
+    -config etc/server.conf \
+    -out "certs/signing/${FILENAME}.csr" \
+    -keyout "certs/signing/${FILENAME}.key"
+  sign_server "${1}"
+}
+
+## Private: Sign a mTLS client certificate CSR
+#
+# Usage:
+#   sign_client commonName
+# Example:
+#   sign_client Florian-iPhone
+sign_client() {
+  FILENAME="${1/./-}"
+  echo "Signing ${1} mTLS client CSR..."
+  openssl ca \
+    -config etc/mtls-ca.conf \
+    -in "certs/mtls/${FILENAME}.csr" \
+    -out "certs/mtls/${FILENAME}.crt" \
+    -extensions client_ext
+  openssl x509 \
+    -in "certs/mtls/${FILENAME}.crt" \
+    -out "certs/mtls/${FILENAME}.cer" \
+    -outform der
+}
+
+## Create a new mTLS client certificate
+#
+# Usage:
+#   create_client commonName
+# Example:
+#   create_client Florian-iPhone
+create_client() {
+  FILENAME="${1/./-}"
+  echo "Generating ${1} mTLS client CSR..."
+  CN="${1}" \
+  openssl req -new \
+    -config etc/client.conf \
+    -out "certs/mtls/${FILENAME}.csr" \
+    -keyout "certs/mtls/${FILENAME}.key"
+  sign_client "${1}"
+}
+
+## Create a new CRL for the given CA
+#
+# Usage:
+#   create_crl caName
+# Example:
+#   create_crl "signing"
+create_crl() {
+  echo "Generating CRL for ${1} CA..."
+  openssl ca -gencrl \
+    -config "etc/${1}-ca.conf" \
+    -out "crl/${1}-ca.crl" \
+  openssl crl \
+    -in "crl/${1}-ca.crl" \
+    -out "crl/${1}-ca.crl" \
+    -outform der
+}
+
+## Revoke a certificate signed by the given CA
+#
+# Usage:
+#   revoke caName commonName reason
+#   reason is one of: unspecified, keyCompromise, CACompromise, affiliationChanged, superseded, cessationOfOperation, certificateHold,
+#   see https://docs.openssl.org/master/man1/openssl-ca/#crl-options
+# Example:
+#   revoke "signing" "openhab.localnet" "superseded"
+revoke() {
+  serial="$(grep $2 ca/$1-ca/db/$1-ca.db | awk '{print $3}')"
+  echo "Revoking certificate for ${2} from ${1} CA due to ${3}..."
+  openssl ca \
+    -config "etc/${1}-ca.conf" \
+    -revoke "ca/${1}-ca/${serial}.pem" \
+    -crl_reason "${3}"
+}
+
+renew_server() {
+  FILENAME="${1/./-}"
+  revoke "signing" "${1}" "superseded"
+  create_crl "signing"
+  sign_server "${1}"
+}
+
+renew_client() {
+  FILENAME="${1/./-}"
+  revoke "mtls" "${1}" "superseded"
+  create_crl "mtls"
+  sign_client "${1}"
+}
+
+## View a certificate
+#
+# Usage:
+#   view_cert commonName
+# Example:
+#   view_cert "openhab.localnet"
+view_cert() {
+  FILENAME="${1/./-}"
+  openssl x509 \
+    -in "certs/${FILENAME}.crt" \
+    -noout \
+    -text
+}
+
+## View a CA certificate
+#
+# Usage:
+#   view_ca_cert caName
+# Example:
+#   view_ca_cert "signing"
+view_ca_cert() {
+  openssl x509 \
+    -in "ca/${1}-ca.crt" \
+    -noout \
+    -text
+}
+
+## View the certificates of a given CA
+#
+# Usage:
+#   view_certs_of_ca caName
+# Example:
+#   view_certs_of_ca "signing"
+view_certs_of_ca() {
+  cat "ca/${1}-ca/db/${1}-ca.db"
+}
+
+generate_client_p12() {
+  FILENAME="${1/./-}"
+  echo -e "\nGenerating p12 bundle for ${1} ...\n"
+  openssl pkcs12 -export -out "p12/${FILENAME}.p12" -inkey "certs/mtls/${FILENAME}.key" -in "certs/mtls/${FILENAME}.crt"
+}
+
+generate_client_p12_ios() {
+  FILENAME="${1/./-}"
+  echo -e "\nGenerating p12 bundle for ${1} for iOS ...\n"
+  # Adding -legacy -certpbe pbeWithSHA1And40BitRC2-CBC for iOS compatibility, but breaks Chromium compatibility!
+  openssl pkcs12 -export -legacy -certpbe pbeWithSHA1And40BitRC2-CBC -out "p12/${FILENAME}.p12" -inkey "certs/mtls/${FILENAME}.key" -in "certs/mtls/${FILENAME}.crt"
+}
+
+$@
